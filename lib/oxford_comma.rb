8e33fd53b4ba0779f97d859d1becc4e6ca259306
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-12 19:10:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,7 @@ def oxford_comma(array)
     return array.join(",")
   elsif array.length == 2
     return array.join(" and ")
-  end
+  elsif array.length > 2
+    array[-1] = "and #{array[-1]}"
+    return array.join(",")
 end
